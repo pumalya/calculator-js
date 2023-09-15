24a72acbe4659943b74442c95bcb956d8b29f79a
Agustin <agustinxmtz@gmail.com>
2023-09-14 19:04:28 -0500

    adding buttons

---
+++ script.js
@@ -0,0 +1,17 @@
+let displayValue = "";
+function calculate(){
+    displayValue = eval(displayValue);
+    document.querySelector('#display').value = displayValue
+
+
+}
+function handleConcat(value){
+    displayValue+=value;
+    document.querySelector('#display').value = displayValue
+}
+function clearDisplay(){
+
+    document.querySelector('#display').value = "";
+
+
+}
